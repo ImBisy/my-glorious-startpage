e2486aff17756639156d15ad9ae10f3012bfa5ac
ImBisy <dragonblade693@gmail.com>
2025-10-30 21:34:22 +0000

    Update clock.js

--- js/clock.js
+++ js/clock.js
@@ -1,42 +1,54 @@
 class Clock {
+    constructor() {
+        this._clockEl = document.querySelector('#clock');
+        this._showSeconds = false; // Toggle state
+        this._setTime = this._setTime.bind(this);
+        this._handleClick = this._handleClick.bind(this); // Click handler
+        this._startClock();
+        this._clockEl.addEventListener('click', this._handleClick);
+    }
 
-	constructor() {
-		this._clockEl = document.querySelector('#clock');
-		this._setTime = this._setTime.bind(this);
-		this._startClock();
-	}
+    // Append 0 before time elements if less hour's than 10
+    _appendZero = k => {
+        if (k < 10) {
+            return '0' + k;
+        } else {
+            return k;
+        }
+    }
 
-	// Append 0 before time elements if less hour's than 10
-	_appendZero = k => {
-		if (k < 10) {
-			return '0' + k;
-		} else {
-			return k;
-		}
-	}
+    _handleClick = () => {
+        this._showSeconds = !this._showSeconds; // Toggle state
+        this._setTime(); // Immediate update
+    }
 
-	_setTime = () => {
-		// Date object
-		const date = new Date();
+    _setTime = () => {
+        // Date object
+        const date = new Date();
 
-		// Set hour, minute
-		let hour = date.getHours();
-		let min = date.getMinutes();
-		let midDay = 'AM';
-		
-		// Assigning
-		midDay = (hour >= 12) ? 'PM' : 'AM';
-		hour = (hour === 0) ? 12 : ((hour > 12) ? (hour - 12) : hour);
-		hour = this._appendZero(hour);
-		min = this._appendZero(min);
+        // Set hour, minute, second
+        let hour = date.getHours();
+        let min = date.getMinutes();
+        let sec = date.getSeconds();
+        let midDay = 'AM';
+        
+        // Assigning
+        midDay = (hour >= 12) ? 'PM' : 'AM';
+        hour = (hour === 0) ? 12 : ((hour > 12) ? (hour - 12) : hour);
+        hour = this._appendZero(hour);
+        min = this._appendZero(min);
+        sec = this._appendZero(sec);
 
-		// Update clock id element
-		this._clockEl.innerText = `${hour}:${min} ${midDay}` ;
-	}
+        // Conditional display with/without seconds
+        if (this._showSeconds) {
+            this._clockEl.innerText = `${hour}:${min}:${sec} ${midDay}`;
+        } else {
+            this._clockEl.innerText = `${hour}:${min} ${midDay}`;
+        }
+    }
 
-	_startClock = () => {
-		this._setTime();
-		setInterval(this._setTime, 1000);
-	}
-
+    _startClock = () => {
+        this._setTime();
+        setInterval(this._setTime, 1000);
+    }
 }
